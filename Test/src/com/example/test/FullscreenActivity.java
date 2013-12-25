53e320397175cd15b6a740e59e85c87421b39960
= <=>
2013-12-26 00:31:54 +0800

    test

--- Test/src/com/example/test/FullscreenActivity.java
+++ Test/src/com/example/test/FullscreenActivity.java
@@ -10,6 +10,7 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+/** Just to test this **/
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
