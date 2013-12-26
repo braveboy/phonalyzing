7ad59a64b037b8d49400b1f2a6f45f51e34fe3ec
= <=>
2013-12-26 23:01:25 +0800

    qq

--- Test/src/com/example/test/FullscreenActivity.java
+++ Test/src/com/example/test/FullscreenActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
   I don't know what to say!
   It's amazing!
   jfalskdjlaksdjflaksdjf
+  ÇòÇòÊÇ¸ö´ó»µµ°
  */
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
